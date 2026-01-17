dc916081e0dd8a1fcda851cddc52b9c14f223201
precision-sys <tiana2412@gmail.com>
2026-01-17 21:29:47 +0300

    mission statement II

---
+++ mission/mission-statement.js
@@ -0,0 +1,33 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {document.body.style.backgroundColor = '#333';
+        document.querySelector('img').src = 'byui-logo-white.png';
+
+        document.querySelector('h1').style.color = 'white';
+
+
+        document.querySelectorAll('p').forEach (p=>p.style.color = 'white');
+        document.querySelectorAll('li').forEach(li=>li.style.color = 'white');
+
+
+        
+
+        // code for changes to colors and logo
+    } else if (current == 'light') {document.body.style.backgroundColor = 'white';
+
+        document.querySelector('img').src = 'byui-logo-blue.webp';
+
+        document.querySelector('h1').style.color = 'black';
+        document.querySelectorAll('p').forEach (p=>p.style.color = 'black');
+
+        document.querySelectorAll('li').forEach(li=>li.style.color = 'black');
+        // code for changes to colors and logo
+    }
+}           
+                    
