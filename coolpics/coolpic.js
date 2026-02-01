27122c12894555dd19b8e9bd0af7379be210f14e
precision-sys <tiana2412@gmail.com>
2026-02-01 08:41:23 +0300

    coolpics part2

---
+++ coolpics/coolpic.js
@@ -0,0 +1,50 @@
+
+
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    console.log(e.target);
+
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('coolpic', 'norris-full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+    modal.showModal();
+
+
+    
+// Code to show modal  - Use event parameter 'e'   
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
+
+
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+menu.classList.toggle('seen');
+}
