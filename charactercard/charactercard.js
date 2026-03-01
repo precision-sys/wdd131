8d1d40136c2867dd79f23f0ad358bec1fe4585db
precision-sys <tiana2412@gmail.com>
2026-03-01 11:08:21 +0300

    final

--- charactercard/charactercard.js
+++ charactercard/charactercard.js
@@ -1,8 +1,29 @@
 const character = {
     name: 'Snortleblat',
     photo: 'images/snortleblat.png',
+    health: 100,
+    attack: 20,
+    level: 5,
+    levelUp: function () {
+        this.level++;
+    }
 }
 
 document.querySelector('.name').textContent = character.name;
 document.querySelector('.image').setAttribute('src', character.photo);
 document.querySelector('.image').setAttribute('alt', character.name);
+document.getElementById('health').textContent = `Health: ${character.health}`;
+document.getElementById('level').textContent = `Level: ${character.level}`;
+
+const attackButton = document.getElementById('attackbut');
+attackButton.addEventListener('click', function () {
+    character.health -= character.attack;
+    document.getElementById('health').textContent = `Health: ${character.health}`;
+});
+
+const levelUpButton = document.getElementById('levelUpbut');
+levelUpButton.addEventListener('click', function () {
+    character.levelUp();
+    document.getElementById('level').textContent = `Level: ${character.level}`;
+    document.getElementById('health').textContent = `Health: ${character.health}`;
+});
