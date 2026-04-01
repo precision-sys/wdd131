608933ae4314fa0e9bed74fd45a6d3bdeffd4c0b
precision-sys <tiana2412@gmail.com>
2026-04-01 19:05:20 +0300

    gsourcing loading...

--- gsourcing/gsourcing.js
+++ gsourcing/gsourcing.js
@@ -1,38 +1,129 @@
 
+const menuBtn = document.querySelector('.menu-btn');
+menuBtn.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+window.addEventListener("resize", function() {
+    const navLinks = document.querySelector('.nav-links');
+    if (window.innerWidth > 768) {
+        navLinks.classList.remove('active')
+    }});
+
+
+
+
+const slides = [
+ {
+    image: "images/slide1.jpg",
+    alt_text: "Business team working together"
+
+ },
+
+ {
+    image: "images/slide2.jpg",
+    alt_text: "Files with paper work"
+
+ },
+
+ {
+    image: "images/slide3.jpg",
+    alt_text: "Tax forms and a calculator on a desk"
+
+ },
+
+ {
+    image: "images/slide4.jpg",
+    alt_text: "Hand holding a pen and signing a contract"
+
+ },
+
+ {
+    image: "images/slide5.jpg",
+    alt_text: "A laptop with accounting documents"
+
+ }
+
+
+]
+
+const slideshow = document.getElementsByClassName("slideshow-container")[0];
+const dotsContainer = document.getElementsByClassName("dots")[0];  
+
+let slidesHTML = "";
+let dotsHTML = "";
+
+slides.forEach((slide, index) => {
+    slidesHTML += `
+        <div class="mySlides fade">
+            <img src="${slide.image}" alt="${slide.alt_text}">
+        </div>
+    `;
+    dotsHTML += `
+        <span class="dot" onclick="currentSlide(${index + 1})"></span>
+    `;
+});
+slideshow.innerHTML += slidesHTML;
+dotsContainer.innerHTML = dotsHTML;
+
 
 let slideIndex = 1;
 window.onload = function() {
     showSlides(slideIndex);
 }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
+
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) slideIndex = 1
+  if (n < 1) slideIndex = slides.length
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].classList.remove("active");
   }
 
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].classList.add("active");
 }
 
-setInterval(function() {
-    plusSlides(1);
-}, 4000);
+
+
+showSlides(slideIndex); 
+
+setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
