406c4f6a6f3ed67d7b8aac294c5cc5982f5336f3
precision-sys <tiana2412@gmail.com>
2026-04-04 17:18:10 +0300

    loading speed

--- gsourcing/gsourcing.js
+++ gsourcing/gsourcing.js
@@ -154,7 +154,7 @@ let dotsHTML = "";
 slides.forEach((slide, index) => {
     slidesHTML += `
         <div class="mySlides fade">
-            <img src="${slide.image}" alt="${slide.alt_text}">
+            <img src="${slide.image}" alt="${slide.alt_text}" loading="lazy">
         </div>
     `;
     dotsHTML += `
